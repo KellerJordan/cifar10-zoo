619c9769d551ecc56c5401deed83f69f54fd6e38
KellerJordan <kellerjordan1998@gmail.com>
2023-12-15 22:47:09 +0000

    big refactor

--- dawn.py
+++ dawn.py
@@ -111,8 +111,6 @@ from itertools import chain, count, islice as take
 ## dict utils
 #####################
 
-union = lambda *dicts: {k: v for d in dicts for (k, v) in d.items()}
-
 make_tuple = lambda path: (path,) if isinstance(path, str) else path
 
 def path_iter(nested_dict, pfx=()):
@@ -285,13 +283,6 @@ def to_numpy(x):
     if isinstance(x, torch.Tensor):
         return x.detach().cpu().numpy()  
     return x
-  
-def flip_lr(x):
-    if isinstance(x, torch.Tensor):
-        return torch.flip(x, [-1]) 
-    return x[..., ::-1].copy()
-  
-trainable_params = lambda model: {k:p for k,p in model.named_parameters() if p.requires_grad}
 
 #####################
 ## Optimisers
@@ -322,7 +313,7 @@ def opt_step(update, param_schedule, step_number, weights, opt_state):
     return {'update': update, 'param_schedule': param_schedule, 'step_number': step_number, 'weights': weights,  'opt_state': opt_state}
 
 SGD = partial(optimiser, update=nesterov_update, state_init=zeros_like)
-  
+
 class PiecewiseLinear(namedtuple('PiecewiseLinear', ('knots', 'vals'))):
     def __call__(self, t):
         return np.interp([t], self.knots, self.vals)[0]
@@ -359,8 +350,6 @@ def table_formatter(val, is_title=False, col_width=12, formats=None):
     type_ = lambda val: float if isinstance(val, (float, np.float)) else type(val)
     return (formats['title'] if is_title else formats.get(type_(val), formats['default'])).format(val, w=col_width)
 
-every = lambda n, col: (lambda data: data[col] % n == 0)
-
 class Table():
     def __init__(self, keys=None, report=(lambda data: True), formatter=table_formatter):
         self.keys, self.report, self.formatter = keys, report, formatter
@@ -377,101 +366,6 @@ class Table():
             
     def df(self):
         return pd.DataFrame([{'_'.join(p): v for p,v in path_iter(row)} for row in self.log])     
-            
-def reduce(batches, state, steps):
-    #state: is a dictionary
-    #steps: are functions that take (batch, state)
-    #and return a dictionary of updates to the state (or None)
-    
-    for batch in chain(batches, [None]): 
-    #we send an extra batch=None at the end for steps that 
-    #need to do some tidying-up (e.g. log_activations)
-        for step in steps:
-            updates = step(batch, state)
-            if updates:
-                for k,v in updates.items():
-                    state[k] = v                  
-    return state
-  
-#define keys in the state dict as constants
-MODEL = 'model'
-VALID_MODEL = 'valid_model'
-OUTPUT = 'output'
-OPTS = 'optimisers'
-ACT_LOG = 'activation_log'
-WEIGHT_LOG = 'weight_log'
-
-#step definitions
-def forward(training_mode):
-    def step(batch, state):
-        if not batch: return
-        model = state[MODEL] if training_mode or (VALID_MODEL not in state) else state[VALID_MODEL]
-        if model.training != training_mode: #without the guard it's slow!
-            model.train(training_mode)
-        return {OUTPUT: model.loss(model(batch))}
-    return step
-
-def forward_tta(tta_transforms):
-    def step(batch, state):
-        if not batch: return
-        model = state[MODEL] if (VALID_MODEL not in state) else state[VALID_MODEL]
-        if model.training:
-            model.train(False)
-        logits = torch.mean(torch.stack([model({'input': transform(batch['input'].clone())})['logits'].detach() for transform in tta_transforms], dim=0), dim=0)
-        return {OUTPUT: model.loss(dict(batch, logits=logits))}
-    return step
-
-def backward(dtype=torch.float16):
-    def step(batch, state):
-        state[MODEL].zero_grad()
-        if not batch: return
-        state[OUTPUT]['loss'].to(dtype).sum().backward()
-    return step
-
-def opt_steps(batch, state):
-    if not batch: return
-    return {OPTS: [opt_step(**opt) for opt in state[OPTS]]}
-
-def log_activations(node_names=('loss', 'acc')):
-    logs = []
-    def step(batch, state):
-        if batch:
-            logs.extend((k, state[OUTPUT][k].detach()) for k in node_names)
-        else:
-            res = map_values((lambda xs: to_numpy(torch.cat(xs)).astype(np.float)), group_by_key(logs))
-            logs.clear()
-            return {ACT_LOG: res}
-    return step
-
-def update_ema(momentum, update_freq=1):
-    n = iter(count())
-    rho = momentum**update_freq
-    def step(batch, state):
-        if not batch: return
-        if (next(n) % update_freq) != 0: return
-        for (k, v), ema_v in zip(state[MODEL].state_dict().items(), state[VALID_MODEL].state_dict().values()):
-            if 'num_batches_tracked' in k:
-                continue
-            ema_v *= rho
-            ema_v += (1-rho)*v
-    return step
-
-train_steps = (forward(training_mode=True), log_activations(('loss', 'acc')), backward(), opt_steps)
-valid_steps = (forward(training_mode=False), log_activations(('loss', 'acc')))
-
-epoch_stats = lambda state: {k: np.mean(v) for k, v in state[ACT_LOG].items()}
-
-def train_epoch(state, timer, train_batches, valid_batches, train_steps=train_steps, valid_steps=valid_steps, on_epoch_end=identity):
-    train_summary, train_time = epoch_stats(on_epoch_end(reduce(train_batches, state, train_steps))), timer()
-    valid_summary, valid_time = epoch_stats(reduce(valid_batches, state, valid_steps)), timer(update_total=False) #DAWNBench rules
-    return {
-        'train': union({'time': train_time}, train_summary), 
-        'valid': union({'time': valid_time}, valid_summary), 
-        'total time': timer.total_time
-    }
-
-summary = lambda logs, cols=['valid_acc']: logs.df().query('epoch==epoch.max()')[cols].describe().transpose().astype({'count': int})[
-    ['count', 'mean', 'min', 'max', 'std']]
 
 #####################
 ## Network
@@ -531,8 +425,6 @@ label_smoothing_loss = lambda alpha: Network({
 ## Misc
 #####################
 
-lr_schedule = lambda knots, vals, batch_size: PiecewiseLinear(np.array(knots)*len(train_loader), np.array(vals)/batch_size)
-
 def cov(X):
     X = X/np.sqrt(X.size(0) - 1)
     return X.t() @ X
@@ -553,7 +445,7 @@ train_aug = dict(flip=True, translate=4)
 train_loader = PrepadCifarLoader('/tmp/cifar10', train=True, batch_size=512, aug=train_aug)
 test_loader = PrepadCifarLoader('/tmp/cifar10', train=False, batch_size=1000)
 
-test_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in test_loader]
+valid_batches = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in test_loader]
 
 train_images = train_loader.normalize(train_loader.images)[:10000]
 
@@ -562,8 +454,8 @@ train_images = train_loader.normalize(train_loader.images)[:10000]
 def whitening_block(c_in, c_out, Λ=None, V=None, eps=1e-2):
     filt = nn.Conv2d(3, 27, kernel_size=(3,3), padding=(1,1), bias=False)
     filt.weight.data = (V/torch.sqrt(Λ+eps)[:,None,None,None])
-    filt.weight.requires_grad = False 
-                                   
+    filt.weight.requires_grad = False
+    
     return {
         'whiten': (identity, {'value': filt}),
         'conv': conv(27, c_out, kernel_size=(1, 1), bias=False),
@@ -578,82 +470,96 @@ input_whitening_net = network(conv_pool_block=conv_pool_block_pre, prep_block=pa
 
 from tqdm import tqdm
 
-valid_steps_tta = (forward_tta([identity, flip_lr]), log_activations(('loss', 'acc')))
+epochs = 10
+batch_size = 512
+ema_epochs = 2
 
+lr_schedule = lambda knots, vals, batch_size: PiecewiseLinear(np.array(knots)*len(train_loader), np.array(vals)/batch_size)
 
-epochs, batch_size, ema_epochs=10, 512, 2
-opt_params = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 1.0, 0.1], batch_size), 'weight_decay': Const(5e-4*batch_size), 'momentum': Const(0.9)}
-opt_params_bias = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 1.0*64, 0.1*64], batch_size), 'weight_decay': Const(5e-4*batch_size/64), 'momentum': Const(0.9)}
+opt_params = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 1.0, 0.1], batch_size),
+              'weight_decay': Const(5e-4*batch_size),
+              'momentum': Const(0.9)}
+opt_params_bias = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 1.0*64, 0.1*64], batch_size),
+                   'weight_decay': Const(5e-4*batch_size/64),
+                   'momentum': Const(0.9)}
 
-logs = Table(report=every(epochs,'epoch'))
+epoch_logs = Table(report=lambda data: data['epoch'] % epochs == 0)
+
 for run in range(3):
     model = build_model(input_whitening_net, label_smoothing_loss(0.2))
-    is_bias = group_by_key(('bias' in k, v) for k, v in trainable_params(model).items())
-    state, timer = {MODEL: model, VALID_MODEL: copy.deepcopy(model), OPTS: [SGD(is_bias[False], opt_params), SGD(is_bias[True], opt_params_bias)]}, Timer(torch.cuda.synchronize)
+    bias_params = [p for k, p in model.named_parameters() if p.requires_grad and 'bias' in k]
+    nonbias_params = [p for k, p in model.named_parameters() if p.requires_grad and 'bias' not in k]
+    
+    nonbias_opt = SGD(nonbias_params, opt_params)
+    bias_opt = SGD(bias_params, opt_params_bias)
+    
+    ema_model = copy.deepcopy(model)
+    
+    momentum = 0.99
+    update_freq = 5
+    rho = momentum**update_freq
+    iter_counter = count()
+    
+    timer = Timer(torch.cuda.synchronize)
+    
     for epoch in tqdm(range(epochs)):
-        train_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in train_loader]
-        logs.append(union({'run': run+1, 'epoch': epoch+1}, train_epoch(state, timer, train_batches2, test_batches2, 
-                                                                        train_steps=(*train_steps, update_ema(momentum=0.99, update_freq=5)),
-                                                                        valid_steps=valid_steps_tta)))
-print(summary(logs))
-
-
-train_aug = dict(flip=True, translate=4, cutout=12)
-train_loader = PrepadCifarLoader('/tmp/cifar10', train=True, batch_size=512, aug=train_aug)
-
-x_ent_loss = Network({
-  'loss':  (nn.CrossEntropyLoss, {'reduction': 'none'}, ['logits', 'target']),
-  'acc': (Correct, {}, ['logits', 'target'])
-})
-
-valid_steps_tta = (forward_tta([identity, flip_lr]), log_activations(('loss', 'acc')))
-
-def train_epoch_tta(state, timer, train_batches, valid_batches, train_steps=train_steps, 
-                    valid_steps=valid_steps, valid_steps_tta=valid_steps_tta, on_epoch_end=identity):
-    train_summary, train_time = epoch_stats(on_epoch_end(reduce(train_batches, state, train_steps))), timer()
-    valid_summary, valid_time = epoch_stats(reduce(valid_batches, state, valid_steps)), timer(update_total=False) #DAWNBench rules
-    valid_summary_tta, valid_time_tta = epoch_stats(reduce(valid_batches, state, valid_steps_tta)), timer(update_total=False) #DAWNBench rules
-    return {
-        'train': union({'time': train_time}, train_summary), 
-        'valid': union({'time': valid_time}, valid_summary), 
-        'tta': union({'time': valid_time}, valid_summary_tta), 
-        'total time': timer.total_time
-    }
-
-#baseline model
-logs = Table()
-for run in tqdm(range(0)):
-    for epochs in [24, 40, 60, 80]:
-        opt_params = {'lr': lr_schedule([0, 5, epochs], [0.0, 0.4, 0.0], batch_size), 'weight_decay': Const(5e-4*batch_size), 'momentum': Const(0.9)}
-
-        model = build_model(network(), x_ent_loss)
-        state, timer = {MODEL: model, OPTS: [SGD(trainable_params(model).values(), opt_params)]}, Timer(torch.cuda.synchronize)
-        for epoch in range(epochs-1):
-            train_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in train_loader]
-            train_epoch(state, timer, train_batches2, test_batches2)
-        train_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in train_loader]
-        logs.append(union({'run': run+1, 'epoch': epochs, 'experiment': 'baseline'}, 
-                          train_epoch_tta(state, timer, train_batches2, test_batches2,
-                                          valid_steps_tta=valid_steps_tta)))
-#final model
-ema_epochs=2
-for run in tqdm(range(3)):
-    for epochs in [24, 40, 60, 80]:
-        opt_params = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 0.4, 0.04], batch_size), 'weight_decay': Const(5e-4*batch_size), 'momentum': Const(0.9)}
-        opt_params_bias = {'lr': lr_schedule([0, epochs/5, epochs - ema_epochs], [0.0, 0.4*64, 0.04
-                                                                          *64], batch_size), 'weight_decay': Const(5e-4*batch_size/64), 'momentum': Const(0.9)}
-
-        model = build_model(input_whitening_net, label_smoothing_loss(0.2))
-        is_bias = group_by_key(('bias' in k, v) for k, v in trainable_params(model).items())
-        state, timer = {MODEL: model, VALID_MODEL: copy.deepcopy(model), OPTS: [SGD(is_bias[False], opt_params), SGD(is_bias[True], opt_params_bias)]}, Timer(torch.cuda.synchronize)
-        for epoch in range(epochs-1):
-            train_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in train_loader]
-            train_epoch(state, timer, train_batches2, test_batches2, train_steps=(*train_steps, update_ema(momentum=0.99, update_freq=5)))
-        train_batches2 = [{'input': inputs.half(), 'target': labels} for (inputs, labels) in train_loader]
-        logs.append(union({'run': run+1, 'epoch': epochs, 'experiment': 'final'}, 
-                          train_epoch_tta(state, timer, train_batches2, test_batches2,
-                                          train_steps=(*train_steps, update_ema(momentum=0.99, update_freq=5)))))
         
-data = logs.df()
-print(data)
+        logs = []
+        node_names = ('loss', 'acc')
+        
+        model.train()
+        for inputs, labels in train_loader:
+            
+            ## forward and logging
+            batch = {'input': inputs.half(), 'target': labels}
+            out = model.loss(model(batch))
+            logs.extend((k, out[k].detach()) for k in node_names)
+            
+            ## backward
+            model.zero_grad()
+            out['loss'].sum().backward()
+            
+            ## optimizer step
+            nonbias_opt = opt_step(**nonbias_opt)
+            bias_opt = opt_step(**bias_opt)
+            
+            ## ema update
+            if next(iter_counter) % update_freq == 0:
+                for (k, v), ema_v in zip(model.state_dict().items(), ema_model.state_dict().values()):
+                    if 'num_batches_tracked' in k:
+                        continue
+                    ema_v *= rho
+                    ema_v += (1-rho)*v
+            
+        res = map_values((lambda xs: to_numpy(torch.cat(xs)).astype(np.float)), group_by_key(logs))
+        train_summary = {k: np.mean(v) for k, v in res.items()}
+
+        train_time = timer()
+        logs.clear()
+
+        ema_model.eval()
+        for inputs, labels in test_loader:
+            batch = {'input': inputs.half(), 'target': labels}
+            
+            with torch.no_grad():
+                inputs = batch['input']
+                logits = ema_model({'input': inputs})['logits']
+                logits_flip = ema_model({'input': inputs.flip(-1)})['logits']
+                out = ema_model.loss(dict(batch, logits=(logits+logits_flip)/2))
+            
+            logs.extend((k, out[k].detach()) for k in node_names)
+
+        res = map_values((lambda xs: to_numpy(torch.cat(xs)).astype(np.float)), group_by_key(logs))
+        valid_summary = {k: np.mean(v) for k, v in res.items()}
+
+        valid_time = timer(update_total=False)
+
+        log = {
+            'train': {'time': train_time, **train_summary}, 
+            'valid': {'time': valid_time, **valid_summary}, 
+            'total time': timer.total_time
+        }
+        
+        epoch_logs.append({'run': run+1, 'epoch': epoch+1, **log})
+
 
