240bb37c05837fc07739824c4cab605147a951f1
KellerJordan <kellerjordan1998@gmail.com>
2023-12-19 03:59:37 +0000

    refactor bn momentum

--- dawn.py
+++ dawn.py
@@ -123,8 +123,8 @@ class PrepadCifarLoader:
 #############################################
 
 class BatchNorm(nn.BatchNorm2d):
-    def __init__(self, num_features, eps=1e-05, momentum=0.1, weight=False, bias=True):
-        super().__init__(num_features, eps=eps, momentum=momentum)
+    def __init__(self, num_features, eps=1e-05, momentum=0.9, weight=False, bias=True):
+        super().__init__(num_features, eps=eps, momentum=1-momentum)
         self.weight.data.fill_(1.0)
         self.bias.data.fill_(0.0)
         self.weight.requires_grad = weight
