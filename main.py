ba306648a1e0bcb0334b6622d9319a6185aadea0
KellerJordan <kellerjordan1998@gmail.com>
2023-12-27 16:39:43 +0000

    refactor

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ def main(run):
     total_train_steps = math.ceil(len(train_loader) * epochs)
     lr_schedule = np.interp(np.arange(1+total_train_steps),
                             [0, int(0.23 * total_train_steps), total_train_steps],
-                            [0.2, 1, 0.07]) # Linear warmup then annealing schedule
+                            [0.2, 1, 0.07]) # triangular learning rate schedule
 
     model = make_net()
     model_ema = None
@@ -331,15 +331,10 @@ def main(run):
 
     params = [(k, p) for k, p in model.named_parameters() if p.requires_grad]
     norm_biases = [p for k, p in params if 'norm' in k]
-    conv_filters = [p for k, p in params if 'conv' in k]
-    whiten_bias = [p for k, p in params if k == '0.bias']
-    linear_weight = [p for k, p in params if k == '7.weight']
-
-    params_unscaled = dict(params=conv_filters+whiten_bias+linear_weight,
-                           lr=lr, weight_decay=(wd / lr))
-    params_scaled   = dict(params=norm_biases,
-                           lr=lr*bias_scaler, weight_decay=(wd / (lr*bias_scaler)))
-    optimizer = torch.optim.SGD([params_unscaled, params_scaled], momentum=momentum, nesterov=True)
+    other_params = [p for k, p in params if 'norm' not in k] # convolutional filters, first layer bias, and final linear layer
+    param_configs = [dict(params=norm_biases, lr=lr*bias_scaler, weight_decay=(wd / (lr*bias_scaler))),
+                     dict(params=other_params, lr=lr, weight_decay=(wd / lr))]
+    optimizer = torch.optim.SGD(param_configs, momentum=momentum, nesterov=True)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_schedule.__getitem__)
 
     ## For accurately timing GPU code
