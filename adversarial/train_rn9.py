9e636d8d8aba992eb4e7b3996e806bd5185abc7b
KellerJordan <kellerjordan1998@gmail.com>
2024-01-23 17:03:23 +0000

    move load to loader

--- adversarial/train_rn9.py
+++ adversarial/train_rn9.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 torch.backends.cudnn.benchmark = True
 
 from loader import CifarLoader
+from model import make_net
 
 hyp = {
     'opt': {
@@ -32,65 +33,6 @@ hyp = {
         'width': 1.0,
     },
 }
-
-#############################################
-#            Network Components             #
-#############################################
-
-def make_net(w=1.0):
-
-    class Mul(nn.Module):
-        def __init__(self, weight):
-            super().__init__()
-            self.weight = weight
-        def forward(self, x):
-            return x * self.weight
-
-    class Flatten(nn.Module):
-        def forward(self, x):
-            return x.view(len(x), -1)
-
-    class Residual(nn.Module):
-        def __init__(self, module):
-            super().__init__()
-            self.module = module
-        def forward(self, x):
-            return x + self.module(x)
-
-    def conv_bn(channels_in, channels_out, kernel_size=3, stride=1, padding=1):
-        bn = nn.BatchNorm2d(channels_out)
-        bn.weight.requires_grad = False
-        return nn.Sequential(
-                nn.Conv2d(channels_in, channels_out,
-                          kernel_size=kernel_size, stride=stride, padding=padding,
-                          bias=False),
-                bn,
-                nn.ReLU(inplace=True)
-        )
-
-    NUM_CLASSES = 10
-    w1 = int(w*64)
-    w2 = int(w*128)
-    w3 = int(w*256)
-    model = nn.Sequential(
-        conv_bn(3, w1, kernel_size=3, stride=1, padding=1),
-        conv_bn(w1, w2, kernel_size=5, stride=2, padding=2),
-        Residual(nn.Sequential(conv_bn(w2, w2), conv_bn(w2, w2))),
-        conv_bn(w2, w3, kernel_size=3, stride=1, padding=1),
-        nn.MaxPool2d(2),
-        Residual(nn.Sequential(conv_bn(w3, w3), conv_bn(w3, w3))),
-        conv_bn(w3, w2, kernel_size=3, stride=1, padding=0),
-        nn.AdaptiveMaxPool2d((1, 1)),
-        Flatten(),
-        nn.Linear(w2, NUM_CLASSES, bias=False),
-        Mul(0.2)
-    )
-    model = model.to(memory_format=torch.channels_last)
-    model = model.cuda()
-    for m in model.modules():
-        if type(m) is not nn.BatchNorm2d:
-            m.half()
-    return model
 
 ########################################
 #           Train and Eval             #
@@ -144,18 +86,6 @@ def train(train_loader):
     log = dict(train_loss=train_loss, train_acc=train_acc, test_acc=test_acc)
     return model, log 
 
-def save_data(loader, path):
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-    obj = {'images': loader.images, 'labels': loader.labels}
-    torch.save(obj, path)
-    
-def load_data(loader, path):
-    obj = torch.load(path)
-    loader.images = obj['images'].half().cuda()
-    loader.labels = obj['labels'].cuda()
-
-
 if __name__ == '__main__':
 
     with open(sys.argv[0]) as f:
@@ -165,7 +95,7 @@ if __name__ == '__main__':
     train_loader = CifarLoader('/tmp/cifar10', train=True, batch_size=hyp['opt']['batch_size'], aug=train_augs)
     if len(sys.argv) >= 2:
         data_path = sys.argv[1]
-        load_data(train_loader, data_path)
+        train_loader.load(data_path)
     else:
         data_path = None
 
