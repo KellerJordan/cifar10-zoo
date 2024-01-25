c0dec25a03835b534da5345e387bce6aed7a79f1
KellerJordan <kellerjordan1998@gmail.com>
2024-01-25 12:05:11 +0000

    .

--- main_basic.py
+++ main_basic.py
@@ -40,7 +40,6 @@ hyp = {
         'weight_decay': 0.0153,     # weight decay per 1024 examples (decoupled from learning rate)
         'bias_scaler': 64.0,        # scales up learning rate (but not weight decay) for BatchNorm biases
         'label_smoothing': 0.2,
-        'whiten_bias_epochs': 3,    # how many epochs to train the whitening layer bias before freezing
     },
     'aug': {
         'flip': True,
