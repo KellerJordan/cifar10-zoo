37f47e3632885b3d8ba669ebfc11471999a0b7d7
KellerJordan <kellerjordan1998@gmail.com>
2024-01-29 09:58:49 +0000

    .

--- adversarial/train.py
+++ adversarial/train.py
@@ -57,7 +57,9 @@ def trainval_split(train_loader, frac=0.02):
     train_loader.labels, val_loader.labels = train_loader.labels[~mask], train_loader.labels[mask]
     return train_loader, val_loader
 
-def train(train_loader, test_loader=None, epochs=hyp['opt']['epochs'], lr=hyp['opt']['lr'], val_split=False):
+def train(train_loader, test_loader=None,
+          epochs=hyp['opt']['epochs'], lr=hyp['opt']['lr'],
+          val_split=False):
 
     if val_split:
         train_loader, val_loader = trainval_split(train_loader)
