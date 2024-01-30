b7b66ccba21cea5bb1701df8c8460da505bf318d
KellerJordan <kellerjordan1998@gmail.com>
2024-01-30 12:50:42 +0000

    .

--- data_valuation/experiment2.py
+++ data_valuation/experiment2.py
@@ -1,6 +1,6 @@
 """
-experiment1.py
-Constructs a subset of CIFAR-10 airplane / frog which yields 7% generalization.
+experiment2.py
+Consistently produces a spike
 """
 from loader import CifarLoader
 from train import train, evaluate
@@ -11,19 +11,18 @@ train_aug = dict(flip=True, translate=4)
 
 print('Training weak classifier to use for splitting...')
 loader = convert_binary(CifarLoader('cifar10', train=True, aug=train_aug, drop_last=False))
-model, log = train(loader, test_loader, epochs=2, val_split=False)
-#viz(log)
+model, _ = train(loader, test_loader, epochs=2, val_split=False)
 
 loader = convert_binary(CifarLoader('cifar10', train=True, aug=train_aug))
 loader = repeat_augs(loader, n_epochs=24)
 margins = get_margins(model, loader).float()
 
-q = margins.quantile(0.05)
+q = margins.quantile(0.12)
 print('margin q:', q)
-mask = (margins < q)
+r = 1.0
+mask = (margins < q) & rand_mask_like(margins, r)
 print(mask.float().mean(), mask.sum())
 loader.images = loader.images[mask] 
 loader.labels = loader.labels[mask]
-model, log = train(loader, test_loader, epochs=10, val_split=False)
-#viz(log)
+train(loader, test_loader, epochs=20, val_split=False)
 
