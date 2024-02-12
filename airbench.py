efb1e14020e22ae1b31f90be1858d008117c8249
KellerJordan <kellerjordan1998@gmail.com>
2024-02-12 12:09:20 +0000

    .

--- airbench.py
+++ airbench.py
@@ -93,7 +93,7 @@ hyp = {
         'train_epochs': 9.9,
         'batch_size': 1024,
         'lr': 11.5,                 # learning rate per 1024 examples
-        'momentum': 0.85,           # decay per 1024 examples (e.g. batch_size=512 gives sqrt of this)
+        'momentum': 0.85,
         'weight_decay': 0.0153,     # weight decay per 1024 examples (decoupled from learning rate)
         'bias_scaler': 64.0,        # scales up learning rate (but not weight decay) for BatchNorm biases
         'label_smoothing': 0.2,
