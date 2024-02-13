fd626217f3ab787f23385fbac4b88bb7352bfd50
KellerJordan <kellerjordan1998@gmail.com>
2024-02-13 16:18:47 +0000

    .

--- airbench_ablations/scaling/waterbench.py
+++ airbench_ablations/scaling/waterbench.py
@@ -1,6 +1,6 @@
 # A variant of airbench optimized for time-to-96%.
 # 49s runtime on an A100; 7.46 PFLOPs.
-# Evidence: 96.05 average accuracy in n=100 runs.
+# Evidence: 96.05 average accuracy in n=200 runs.
 # If random flip is used instead of alternating, then decays to 96.01 average accuracy in n=100 runs.
 #
 # Changes relative to airbench:
