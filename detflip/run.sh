8f3bc174a12d362f5a9e715e41faa7704c14a7d0
KellerJordan <kellerjordan1998@gmail.com>
2024-03-01 01:08:18 +0000

    .

--- detflip/run.sh
+++ detflip/run.sh
@@ -1,7 +1,7 @@
 train() {
-python train4.py --config-file rn18_configs/rn18_16_epochs.yaml \
-    --data.train_dataset=/home/ubuntu/ffcv/ffcv-imagenet/imagenet_ffcv/train_400_0.10_90.ffcv \
-    --data.val_dataset=/home/ubuntu/ffcv/ffcv-imagenet/imagenet_ffcv/val_400_0.10_90.ffcv \
+python train4.py --config-file rn18_16_epochs.yaml \
+    --data.train_dataset=./imagenet_ffcv/train_400_0.10_90.ffcv \
+    --data.val_dataset=./imagenet_ffcv/val_400_0.10_90.ffcv \
     --data.num_workers=26 --data.in_memory=1 \
     --logging.folder=./logs/
 }
