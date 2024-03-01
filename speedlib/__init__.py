3368b89de0b75023ed1decabcedc0001afa85829
KellerJordan <kellerjordan1998@gmail.com>
2024-03-01 19:55:28 +0000

    .

--- speedlib/__init__.py
+++ speedlib/__init__.py
@@ -1,3 +1,3 @@
-from .airbench_lite import train as train_lite
-from .airbench_medium import train as train_medium
-from .airbench_heavy import train as train_heavy
+from .airbench_lite import train as train_lite, make_net as make_net_lite
+from .airbench_medium import train as train_medium, make_net as make_net_medium
+from .airbench_heavy import train as train_heavy, make_net as make_net_heavy
