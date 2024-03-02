62b8e72bdc40f1d35b1adc7ce1034b16d01ad326
KellerJordan <kellerjordan1998@gmail.com>
2024-03-02 06:54:30 +0000

    .

--- airbench/speedlib/__init__.py
+++ airbench/speedlib/__init__.py
@@ -1,5 +1,5 @@
-from .airbench_lite import train94, make_net as make_net94
-from .airbench_medium import train95, make_net as make_net95
-from .airbench_heavy import train96, make_net as make_net96
+from .lib_airbench94 import train94, make_net as make_net94
+from .lib_airbench95 import train95, make_net as make_net95
+from .lib_airbench96 import train96, make_net as make_net96
 from .utils import infer, evaluate, PrepadCifarLoader
 
