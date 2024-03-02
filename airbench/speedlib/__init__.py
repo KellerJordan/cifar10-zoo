027763bb21f07434439fb76bcf870e6a9702bcfc
KellerJordan <kellerjordan1998@gmail.com>
2024-03-02 08:58:11 +0000

    .

--- airbench/speedlib/__init__.py
+++ airbench/speedlib/__init__.py
@@ -1,4 +1,3 @@
-#from .lib_airbench92 import train92, make_net as make_net92
 from .lib_airbench93 import train93, make_net as make_net93
 from .lib_airbench94 import train94, make_net as make_net94
 from .lib_airbench95 import train95, make_net as make_net95
