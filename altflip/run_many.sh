e88e22a53048297028487b9c432e78068eafbfdb
KellerJordan <kellerjordan1998@gmail.com>
2024-03-24 09:24:23 +0000

    .

--- altflip/run_many.sh
+++ altflip/run_many.sh
@@ -1,7 +1,7 @@
 train() {
-#script=train4_translate
-#script=train4_noflip
-script=train4
+#script=train4_translate.py
+#script=train4_noflip.py
+script=train4.py
 python $script --config-file rn18_88_epochs.yaml \
     --data.train_dataset=./imagenet_ffcv/train_400_0.10_90.ffcv \
     --data.val_dataset=./imagenet_ffcv/val_400_0.10_90.ffcv \
